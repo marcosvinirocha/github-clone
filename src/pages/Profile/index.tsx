65250c8c4f0426a02a160eb633f72a6e61b49246
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2020-08-09 16:55:36 -0300

    teste

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -25,7 +25,7 @@ interface Data {
 }
 
 const Profile: React.FC = () => {
-  const { username = 'guilhermerodz' } = useParams();
+  const { username = 'marcosvinirocha' } = useParams();
   const [data, setData] = useState<Data>();
 
   useEffect(() => {
